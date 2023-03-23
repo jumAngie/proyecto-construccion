5481a7c165efa5927b888314b81df83895d57cd1
Angie <angie.camposyc03@gmail.com>
2023-03-23 14:29:46 -0600

    Proyecto en VS

---
+++ Construccion/Construccion.DataAccess/RequestStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construccion.DataAccess
+{
+    class RequestStatus
+    {
+        public int CodeStatus { get; set; }
+        public string MessageStatus { get; set; }
+    }
+}
