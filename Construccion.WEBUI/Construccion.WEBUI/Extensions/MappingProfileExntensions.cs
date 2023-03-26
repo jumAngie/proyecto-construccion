7b6cc22686da6c161aa534e6ac23bb6099acac29
AxelRiv <125411077+AxelRivera504@users.noreply.github.com>
2023-03-25 19:37:31 -0600

    Plantilla Cuba

---
+++ Construccion.WEBUI/Construccion.WEBUI/Extensions/MappingProfileExntensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Construccion.WEBUI.Extensions
+{
+    public class MappingProfileExntensions : Profile
+    {
+        public MappingProfileExntensions()
+        {
+            #region Roles
+            CreateMap<RolesViewModel, WV_tbRoles>().ReverseMap();
+            CreateMap<tbRoles, RolesViewModel>().ReverseMap();
+            #endregion
+
+            #region Usuarios
+            CreateMap<UsuariosViewModel, WV_tbUsuarios>().ReverseMap();
+            CreateMap<tbUsuarios, UsuariosViewModel>().ReverseMap();
+            #endregion
+
+        }
+    }
+}
