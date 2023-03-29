af6f0a3002181483b2c30c41f97a736e31e07152
Angie Campos <angie.camposyc03@gmail.com>
2023-03-28 22:52:48 -0600

    Meti los procs de Insumos

--- BD Ciclo de Proyectos Construcción.sql
+++ BD Ciclo de Proyectos Construcción.sql
@@ -2008,6 +2008,68 @@ BEGIN
 
 END
 GO
+								--- ***** Insumos ****** ----
+GO
+CREATE OR ALTER PROC Cons.UDP_tbInsumos_Insert
+	@insm_Descripcion			NVARCHAR(200), 
+	@unim_Id					INT, 
+	@user_UsuCreacion			INT,
+	@status						INT OUTPUT
+AS
+BEGIN
+
+	BEGIN TRY
+		INSERT INTO Cons.tbInsumos(insm_Descripcion, unim_Id, user_UsuCreacion)
+		VALUES					  (@insm_Descripcion, @unim_Id,@user_UsuCreacion);
+		SET @status = 1;
+	END TRY
+	BEGIN CATCH
+		SET @status = 0;
+	END CATCH;
+END
+GO
+CREATE OR ALTER PROC Cons.UDP_tbInsumos_Update
+	@insm_Id					INT,
+	@insm_Descripcion			NVARCHAR(200), 
+	@unim_Id					INT, 
+	@user_UsuModificacion		INT,
+	@status						INT OUTPUT
+AS
+BEGIN	
+	BEGIN TRY
+		UPDATE Cons.tbInsumos
+		SET		insm_Descripcion = @insm_Descripcion,
+				unim_Id = @unim_Id,
+				user_UsuModificacion = @user_UsuModificacion
+		WHERE	insm_Id = @insm_Id
+		SET @status = 1;
+	END TRY
+	BEGIN CATCH
+		SET @status = 0;
+	END CATCH;
+END
+
+GO
+CREATE OR ALTER PROC Cons.UDP_tbInsumos_Delete
+	@insm_Id					INT,
+	@user_UsuModificacion		INT,
+	@status						INT OUTPUT
+AS
+BEGIN	
+
+	BEGIN TRY
+	   UPDATE Cons.tbInsumos
+	   SET    user_Estado = 0,
+			  user_UsuModificacion = @user_UsuModificacion
+	   WHERE  insm_Id = @insm_Id
+		SET @status = 1;
+	END TRY
+	BEGIN CATCH
+		SET @status = 0;
+	END CATCH;
+
+END
+GO
 								--- ***** CLIENTES ****** ----
 CREATE OR ALTER PROC Cons.UDP_tbClientes_Insert
 	@clie_Identificacion		VARCHAR(150), 
