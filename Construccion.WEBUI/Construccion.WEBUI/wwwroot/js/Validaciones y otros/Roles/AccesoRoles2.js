1f1395eaa2d86906a2819f9ad621aa88898b48f7
AxelRiv <125411077+AxelRivera504@users.noreply.github.com>
2023-03-30 07:52:03 -0600

    Tratar de arreglar los pvtos roles
    Hola angie!!X30

--- Construccion.WEBUI/Construccion.WEBUI/wwwroot/js/Validaciones y otros/Roles/AccesoRoles2.js
+++ Construccion.WEBUI/Construccion.WEBUI/wwwroot/js/Validaciones y otros/Roles/AccesoRoles2.js
@@ -1,5 +1,4 @@
 ﻿function AbrirModalAccesos() {
-    LimpiarAcceso();
     $("#ModalAcceso").modal("show");
     $.ajax({
         url: "/Roles/RolesPantalla",
@@ -8,34 +7,55 @@
         success: function (data) {
             console.log(data);
             $.each(data.res, function (i, value) {
+                alert("hola");
                 if (value.pant_Id == 1) {
+                    console.log("activa1");
                     document.getElementById("1").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 2) {
+                    console.log("activa2");
                     document.getElementById("2").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 3) {
+                    console.log("activa3");
                     document.getElementById("3").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 4) {
+                    console.log("activa4");
                     document.getElementById("4").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 5) {
+                    console.log("activa5");
                     document.getElementById("5").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 6) {
+                    console.log("activa6");
                     document.getElementById("6").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 7) {
+                    console.log("activa7");
                     document.getElementById("7").setAttribute('checked', 'true');
                 }
                 if (value.pant_Id == 8) {
+                    console.log("activa8");
                     document.getElementById("8").setAttribute('checked', 'true');
                 }
             });
         }
     });
 }
+
+$("#1").click(function () {
+    if (document.getElementById("1").checked) {
+        alert("activado");
+        alert($("#txtIdRol").val());
+    }
+    else {
+        alert("desactivado");
+        alert($("#txtIdRol").val());
+    }
+});
+
 
 $("#btnAplicar").click(function () {
     window.localtion.reload();
@@ -46,21 +66,13 @@ function CerrarModalAccesos() {
 }
 
 function LimpiarAcceso() {
-    const $miCheckbox = document.querySelector("#1");
-    const $miCheckbox2 = document.querySelector("#2");
-    const $miCheckbox3 = document.querySelector("#3");
-    const $miCheckbox4 = document.querySelector("#4");
-    const $miCheckbox5 = document.querySelector("#5");
-    const $miCheckbox6 = document.querySelector("#6");
-    const $miCheckbox7 = document.querySelector("#7");
-    const $miCheckbox8 = document.querySelector("#8");
-    $miCheckbox.checked = false;
-    $miCheckbox2.checked = false;
-    $miCheckbox3.checked = false;
-    $miCheckbox4.checked = false;
-    $miCheckbox5.checked = false;
-    $miCheckbox6.checked = false;
-    $miCheckbox7.checked = false;
-    $miCheckbox8.checked = false;
+    $("#1").prop("checked", false);
+    $("#2").prop("checked", false);
+    $("#3").prop("checked", false);
+    $("#4").prop("checked", false);
+    $("#5").prop("checked", false);
+    $("#6").prop("checked", false);
+    $("#7").prop("checked", false);
+    $("#8").prop("checked", false);
+    alert("Limpia");
 }
-
